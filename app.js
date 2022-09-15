5a0b5f213c6156cdfb69743b93ab444b519bc5d3
Sheikh sadi <you@example.com>
2022-09-16 00:39:42 +0600

    carousel started

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+
+const images = [
+     'images/pic-1.jpg',
+     'images/pic-2.jpg',
+     'images/pic-3.jpg',
+     'images/pic-4.jpg',
+     'images/pic-5.jpg',
+     'images/pic-6.jpg',
+     'images/pic-7.jpg',
+     'images/pic-8.jpg',
+     'images/pic-9.jpg'
+]
+
+let imageIndex = 0;
+const img = document.getElementById('carousel-image');
+
+setInterval( () =>{
+     if(imageIndex === images.length){
+          imageIndex = 0;
+     }
+
+     const imageLink = images[imageIndex]
+     img.setAttribute('src', imageLink);
+     imageIndex++;
+
+},1000);
